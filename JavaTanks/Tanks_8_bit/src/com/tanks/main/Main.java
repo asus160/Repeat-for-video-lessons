6cc63c47a3d6f8fc9513eff7bddb0601d515c230
asus160 <asus160109@gmail.com>
2019-09-09 22:55:32 +0300

    Create black color in main window and white oval within

--- JavaTanks/Tanks_8_bit/src/com/tanks/main/Main.java
+++ JavaTanks/Tanks_8_bit/src/com/tanks/main/Main.java
@@ -1,5 +1,7 @@
 package com.tanks.main;
-
+import java.awt.event.ActionEvent;
+import javax.swing.Timer;
+import javax.swing.AbstractAction;
 import com.tanks.display.Display;
 
 public class Main {
@@ -7,6 +9,16 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Display.Create(800, 600, "Tanks");
+		
+		Timer t  = new Timer(1000/60, new AbstractAction() {
+			
+			
+			public void actionPerformed(ActionEvent e) {
+				Display.render();		
+			}
+		});
+		
+		t.setRepeats(true);
+		t.restart();
 	}
-
 }
